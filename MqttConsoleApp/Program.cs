227412e105faf5ae5cc36488abad08cef46bf086
Michal-Sitarz <me@michalsitarz.com>
2021-07-29 19:32:24 +0100

    Added "Subscribe" part of the Pub/Sub pattern. Also, code tidy-up (refactoring).

--- MqttConsoleApp/Program.cs
+++ MqttConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Mqtt;
 using System.Text;
 using System.Threading;
@@ -8,41 +9,70 @@ namespace MqttConsoleApp
 {
     class Program
     {
-        const string mqttBrokerIpAddress = "192.168.1.98";
-
         static void Main(string[] args)
         {
-            
+            const string _mqttBroker = "192.168.1.98";
+            List<string> topics = new List<string> { "greenhouse/conditions", "mosTst1" };
+
             Console.WriteLine("Starting application...");
 
-            var mqtt = StartMqttAsync().Result;
+            var mqtt = StartMqttClientAsync(_mqttBroker).Result;
 
             if (mqtt.connected)
             {
                 Console.WriteLine("MQTT broker connection: OK");
 
-                while (true)
+                // subscribe to all topics
+                var subscribed = SubscribeToTopics(topics, mqtt.client).Result;
+                if (subscribed)
                 {
-                    Publish(mqtt.client);
+                    // recieve messages from topics
+                    mqtt.client
+                        .MessageStream
+                        .Subscribe(msg => MessagesMonitoring(msg.Topic, Encoding.UTF8.GetString(msg.Payload)));
+                    //   ^ observable collection, triggered whenever new message is recieved from any topic
                 }
+
+                // uncomment to use the publishing messages in a constant loop (i.e. for testing)
+                //while (true)
+                //{
+                //    Publish(mqtt.client);
+                //}
             }
             else
             {
-                Console.WriteLine("MQTT broker connection: failed");
+                Console.WriteLine("MQTT broker connection: failed attempt.");
             }
 
             Console.WriteLine("Application finished.");
         }
 
+        static void MessagesMonitoring(string topic, string message)
+        {
+            // logic what to do with each type (topic) of a message
+            // i.e. if this topic, then this...
+
+            // for now, for all new messages -> display in console
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("Message: ");
+            Console.ResetColor();
+            Console.Write(message);
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write($"[@ Topic: {topic}]");
+            Console.ResetColor();
+        }
+
         static void Publish(IMqttClient mqttClient)
         {
+            // publish after each second
             Thread.Sleep(1000);
 
+            // testing values
             var now = DateTime.Now;
-            var message = $"MQTT rocks {now}";
-
+            var message = $"MQTT rocks! {now}";
             var topic = "ConsoleTestTopic1";
 
+            // publish message
             var result = PublishMessageAsync(message, topic, mqttClient).Result;
             if (result)
             {
@@ -54,10 +84,10 @@ namespace MqttConsoleApp
             }
         }
 
-        static async Task<(IMqttClient client, bool connected)> StartMqttAsync()
+        static async Task<(IMqttClient client, bool connected)> StartMqttClientAsync(string brokerIpAddress)
         {
             // connect
-            var client = await MqttClient.CreateAsync(mqttBrokerIpAddress, new MqttConfiguration());
+            var client = await MqttClient.CreateAsync(brokerIpAddress, new MqttConfiguration());
             Console.WriteLine("Connecting to the MQTT broker...");
 
             try
@@ -70,7 +100,7 @@ namespace MqttConsoleApp
                 Console.WriteLine("MQTT: not connected. Sorry :(");
                 return (client, false);
             }
-            
+
             return (client, true);
         }
 
@@ -88,5 +118,23 @@ namespace MqttConsoleApp
                 return false;
             }
         }
+
+        static async Task<bool> SubscribeToTopics(List<string> topics, IMqttClient client)
+        {
+            try
+            {
+                foreach (var t in topics)
+                {
+                    await client.SubscribeAsync(t, MqttQualityOfService.ExactlyOnce);
+                }
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
     }
 }
